2dfab5b70495928c7eb6c54701bed208da30491d
NKOU ATEBA Cédric Christian <cedric.nkouateba@gmail.com>
2026-04-16 06:52:33 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// function setLanguage(lang) {
+//   localStorage.setItem("preferredLanguage", lang);
+//   fetch("./assets/translations.json")
+//     .then(response => response.json())
+//     .then(data => {
+//       document.querySelectorAll("[data-i18n]").forEach(el => {
+//         const key = el.getAttribute("data-i18n");
+//         if (data[lang][key]) {
+//           el.textContent = data[lang][key];
+//         }
+//       });
+//     })
+//     .catch(error => console.error("Erreur de chargement des traductions:", error));
+// }
+
 function setLanguage(lang) {
   localStorage.setItem("preferredLanguage", lang);
   fetch("./assets/translations.json")
@@ -32,6 +47,8 @@ function setLanguage(lang) {
     })
     .catch(error => console.error("Erreur de chargement des traductions:", error));
 }
+
+// ----------------------------
 
 document.querySelector('.adhesion-form').addEventListener('submit', function(e) {
     e.preventDefault();
